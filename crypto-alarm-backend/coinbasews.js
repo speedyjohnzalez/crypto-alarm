b912bf3de2f7a3bc2b0423b1843c58c0f20a0152
speedyjohnzalez <johnlemuelgarcia@gmail.com>
2024-12-03 03:36:52 -0600

    Added full names to each crypto coin in the WS

--- crypto-alarm-backend/coinbasews.js
+++ crypto-alarm-backend/coinbasews.js
@@ -8,19 +8,29 @@ function coinbaseModule() {
         .then(response => response.json())
         .then(cryptoData => {
             //console.log(Object.keys(cryptoData))
-            cryptoIds = cryptoData.products.map(cryptoArray => cryptoArray.product_id)
+            cryptoIds = cryptoData.products.map(cryptoArray => cryptoArray.product_id);
+            cryptoNA = cryptoData.products.map(name => ({
+                name: name.base_name,
+                acronym: name.product_id
+            }))
+            //console.log(cryptoName)
             const coinbaseSocket = new Websocket('wss://ws-feed.exchange.coinbase.com')
             coinbaseSocket.on('open', () => {
                 console.log('Connected to Coinbase WS!');
                 coinbaseSocket.send(JSON.stringify({ type: 'subscribe', product_ids: cryptoIds, channels: ['ticker'] }));
                 coinbaseSocket.on('message', (data) => {
                     const parsedData = JSON.parse(data);
-                    const crypto = parsedData.product_id;
+                    const coinAcronym = parsedData.product_id
                     const price = parsedData.price
                     const time = parsedData.time
-                    console.log(`Crypto: ${crypto}, Price: ${price}, Time: ${time}`)
+                    const matchingCrypto = cryptoNA.find(cryptos => cryptos.acronym === coinAcronym);
+                    if (matchingCrypto) {
+                        console.log(`Crypto: ${matchingCrypto.name} : ${coinAcronym}, Price: ${price}, Time: ${time}`)
+                    }
+                    //console.log(`Crypto: ${coinAcronym}, Price: ${price}, Time: ${time}`)
                 })
             })
+            
         })
 }
 
